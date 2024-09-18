16d4981d4ea76c245e258d09b4e7f8677ee73a8f
Tiyana19 <techietiyana@gmail.com>
2024-09-18 19:26:15 +0530

    food-bucket created

---
+++ Food-bucket/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#674636",
+        secondary: "#AAB396",
+        textGreen: "#181C14",
+        bgColor: {
+          100: "#FFF8E8",
+          200: "#F7EED3",
+        },
+        linkColor: "#C96868",
+      },
+      fontFamily: {
+        headFont: ["Nunito"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+//  boxShadow: {
+//         shadow: {
+//           bt: [
+//             "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+//           ],
+//           hv: [
+//             "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+//           ],
+//           in: [
+//             "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset",
+//           ],
+//         },
+//       },
